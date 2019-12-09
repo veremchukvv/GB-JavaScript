0f5935cac2ec49f862af5bdecd3dfef6801d4a8c
veremchukvv <veremchukvv@gmail.com>
2019-12-09 15:17:31 +0300

    Передал цикл на while

--- HW3.js
+++ HW3.js
@@ -1,12 +1,18 @@
 //Первое задание
 
-outer: for (i=2;i<=100;i++) {
+var i = 2;
+outer: while (i<=100) {
         for (z=2;z<=i-1;z++) {
         simple = i % z;
         if (simple === 0) {
             alert(i+" Составное");
+            i++;
             continue outer
         }
     }
     alert(i+" Простое");
+    i++;
     }
+
+//Второе задание
+
